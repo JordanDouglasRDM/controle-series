06a5a6216e4e92eca506f7beb9114427527c4e06
Jordan Douglas <jordandouglas8515@gmail.com>
2024-03-28 00:05:45 -0300

    finalizado aula 4, autenticação.

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,13 @@
-<div id="mySidebar" class="sidebar">
-    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
-    <a href="{{ route('series.index') }}">Home</a>
-    <a href="{{ route('series.create') }}">Nova Série</a>
+<div id="mySidebar" class="sidebar d-flex">
+    <a href="javascript:void(0)" class="closebtn meus-links" onclick="closeNav()">×</a>
+    <a id="btn-side-home" class="meus-links" href="{{ route('series.index') }}">Home</a>
+    @auth()
+        <a href="{{ route('logout') }}" class="btn btn-outline-danger align-self-end mb-5">Sair</a>
+    @endauth
+
+    @guest
+        <a href="{{ route('login') }}" class="btn btn-success align-self-end mb-5">login</a>
+    @endguest
 </div>
 <div id="main">
     <button class="openbtn" onclick="openNav()">☰</button>
@@ -9,6 +15,7 @@
 </div>
 
 <script>
+    currentSelected();
     function openNav() {
         document.getElementById("mySidebar").style.width = "250px";
         document.getElementById("main").style.marginLeft = "250px";
@@ -17,5 +24,27 @@
     function closeNav() {
         document.getElementById("mySidebar").style.width = "0";
         document.getElementById("main").style.marginLeft = "0";
+    }
+    function disableAnchor(idMyAnchor) {
+        let anchor = document.getElementById(idMyAnchor)
+        anchor.classList.remove('active-personalized')
+        anchor.classList.add('meus-links')
+    }
+    function enableAnchor(idMyAnchor) {
+        let anchor = document.getElementById(idMyAnchor)
+        anchor.classList.remove('meus-links')
+        anchor.classList.add('active-personalized')
+    }
+    function currentSelected() {
+        const currentUrl = window.location.href;
+        const currentDirectoryArray = currentUrl.split('/');
+        const positionArray = currentDirectoryArray.length - 1;
+        const currentDirectory = currentDirectoryArray[positionArray];
+
+        if (currentDirectory === 'series') {
+            enableAnchor('btn-side-home');
+        } else {
+            disableAnchor('btn-side-home');
+        }
     }
 </script>
